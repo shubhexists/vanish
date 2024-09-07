6f335fcdc4d337ab296622928477f70ee544d344
Shubham Singh <shubh622005@gmail.com>
2024-09-07 17:14:36 +0530

    feat: sample macos

--- core/src/trust_stores/macos.rs
+++ core/src/trust_stores/macos.rs
@@ -1,249 +1,178 @@
-// use super::errors::TrustStoreError;
-// use colored::*;
-// use openssl::error::ErrorStack;
-// use openssl::x509::{X509Name, X509};
-// use plist::{self, Value};
-// use std::collections::HashMap;
-// use std::fs::{self, File};
-// use std::io::{self, Read, Write};
-// use std::path::{Path, PathBuf};
-// use std::process::{Command, Stdio};
-// use tempfile::NamedTempFile;
+use openssl::error;
+use openssl::x509::X509;
+use plist::{from_bytes, to_writer_xml, Value};
+use std::fs::{self, File};
+use std::io::{self, Read};
+use std::process::Command;
 
-// pub struct CAValue {
-//     pub ca_uniques_name: String,
-//     pub certificate: X509,
-// }
+pub struct CAValue {
+    ca_cert: X509,
+    ca_root: String,
+}
 
-// impl CAValue {
-//     pub fn install_certificate(&self) -> Result<(), TrustStoreError> {
-//         let cert_path = self.get_cert_path()?;
-//         if self.is_certificate_installed(&cert_path) {
-//             println!("{}: Certificate already installed ✅", "Info".blue());
-//             return Ok(());
-//         }
+impl CAValue {
+    pub fn install_platform(&self) -> Result<bool, String> {
+        let cert_path = format!("{}/{}", self.ca_root, "rootCA.pem");
+        let output = Command::new("sudo")
+            .arg("security")
+            .arg("add-trusted-cert")
+            .arg("-d")
+            .arg("-k")
+            .arg("/Library/Keychains/System.keychain")
+            .arg(&cert_path)
+            .output()
+            .map_err(|e| format!("Failed to execute security add-trusted-cert: {}", e))?;
 
-//         self.write_certificate_with_tee(&cert_path)?;
-//         self.update_trust_settings(&cert_path)?;
+        if !output.status.success() {
+            return Err(format!(
+                "Error executing security add-trusted-cert: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ));
+        }
 
-//         println!("{}: Certificate installed successfully!", "Success".green());
-//         Ok(())
-//     }
+        let temp_file_path: &str = "/tmp/trust-settings.plist";
+        let mut temp_file: File = File::create(temp_file_path)
+            .map_err(|e: io::Error| format!("Failed to create temp file: {}", e))?;
 
-//     fn get_cert_path(&self) -> Result<PathBuf, TrustStoreError> {
-//         let root_name = format!("ca_{}.pem", self.ca_uniques_name);
-//         let path = Path::new("/Library/Keychains/System.keychain").join(&root_name);
-//         Ok(path)
-//     }
+        let output = Command::new("sudo")
+            .arg("security")
+            .arg("trust-settings-export")
+            .arg("-d")
+            .arg(temp_file_path)
+            .output()
+            .map_err(|e: io::Error| {
+                format!("Failed to execute security trust-settings-export: {}", e)
+            })?;
 
-//     fn is_certificate_installed(&self, pem_path: &Path) -> bool {
-//         if pem_path.exists() {
-//             println!(
-//                 "{}: Certificate already exists in {}",
-//                 "Info".blue(),
-//                 pem_path.display()
-//             );
-//             true
-//         } else {
-//             false
-//         }
-//     }
+        if !output.status.success() {
+            return Err(format!(
+                "Error executing security trust-settings-export: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ));
+        }
 
-//     fn write_certificate_with_tee(&self, pem_path: &Path) -> Result<(), TrustStoreError> {
-//         let cert_pem = self
-//             .certificate
-//             .to_pem()
-//             .map_err(|err: ErrorStack| TrustStoreError::PEMEncodingError(err))?;
+        let mut plist_data: Vec<u8> = Vec::new();
+        temp_file
+            .read_to_end(&mut plist_data)
+            .map_err(|e: io::Error| format!("Failed to read trust settings: {}", e))?;
 
-//         let mut tee_cmd = Command::new("sudo")
-//             .arg("tee")
-//             .arg(pem_path.to_str().unwrap())
-//             .stdin(Stdio::piped())
-//             .stdout(Stdio::null())
-//             .spawn()
-//             .map_err(|err| {
-//                 TrustStoreError::CommandError(format!(
-//                     "{}: Failed to run sudo tee: {}",
-//                     "Error".red(),
-//                     err
-//                 ))
-//             })?;
+        let mut plist_root: Value = from_bytes(&plist_data)
+            .map_err(|e: plist::Error| format!("Failed to parse trust settings plist: {}", e))?;
 
-//         tee_cmd
-//             .stdin
-//             .as_mut()
-//             .ok_or(TrustStoreError::CommandError(
-//                 "Failed to open tee stdin".to_string(),
-//             ))?
-//             .write_all(&cert_pem)
-//             .map_err(|err: io::Error| {
-//                 TrustStoreError::PEMFileCreationError(io::Error::new(io::ErrorKind::Other, err))
-//             })?;
+        let root_subject_asn1: Vec<u8> = self
+            .ca_cert
+            .subject_name()
+            .to_der()
+            .map_err(|e: error::ErrorStack| format!("Failed to marshal CA subject: {}", e))?;
 
-//         let status = tee_cmd.wait().map_err(|err| {
-//             TrustStoreError::CommandError(format!(
-//                 "{}: Failed to wait for tee: {}",
-//                 "Error".red(),
-//                 err
-//             ))
-//         })?;
+        if plist_root.as_dictionary().unwrap()["trustVersion"]
+            .as_unsigned_integer()
+            .unwrap()
+            != 1
+        {
+            return Err("ERROR: unsupported trust settings version".to_string());
+        }
 
-//         if !status.success() {
-//             return Err(TrustStoreError::CommandError(
-//                 "Tee command failed".to_string(),
-//             ));
-//         }
+        let trust_list = plist_root
+            .as_dictionary_mut()
+            .unwrap()
+            .get_mut("trustList")
+            .unwrap()
+            .as_dictionary_mut()
+            .unwrap();
 
-//         Ok(())
-//     }
+        for (_key, entry_value) in trust_list.iter_mut() {
+            let entry = entry_value.as_dictionary_mut().unwrap();
+            if let Some(issuer_name) = entry.get("issuerName") {
+                if issuer_name.as_data().unwrap() == &root_subject_asn1 {
+                    entry.insert(
+                        "trustSettings".to_string(),
+                        Value::Array(vec![
+                            Value::Dictionary({
+                                let mut dict = plist::Dictionary::new();
+                                dict.insert(
+                                    "kSecTrustSettingsPolicy".to_string(),
+                                    Value::Data(vec![
+                                        0x28, 0xA3, 0x48, 0x86, 0xF7, 0x63, 0x64, 0x01,
+                                    ]),
+                                );
+                                dict.insert(
+                                    "kSecTrustSettingsPolicyName".to_string(),
+                                    Value::String("sslServer".to_string()),
+                                );
+                                dict.insert(
+                                    "kSecTrustSettingsResult".to_string(),
+                                    Value::Integer(1_i64.into()),
+                                );
+                                dict
+                            }),
+                            Value::Dictionary({
+                                let mut dict = plist::Dictionary::new();
+                                dict.insert(
+                                    "kSecTrustSettingsPolicy".to_string(),
+                                    Value::Data(vec![
+                                        0x28, 0xA3, 0x48, 0x86, 0xF7, 0x63, 0x64, 0x02,
+                                    ]),
+                                );
+                                dict.insert(
+                                    "kSecTrustSettingsPolicyName".to_string(),
+                                    Value::String("basicX509".to_string()),
+                                );
+                                dict.insert(
+                                    "kSecTrustSettingsResult".to_string(),
+                                    Value::Integer(1.into()),
+                                );
+                                dict
+                            }),
+                        ]),
+                    );
+                    break;
+                }
+            }
+        }
 
-//     fn update_trust_settings(&self, cert_path: &Path) -> Result<(), TrustStoreError> {
-//         let mut temp_file = NamedTempFile::new().map_err(|err| {
-//             TrustStoreError::CommandError(format!("Failed to create temp file: {}", err))
-//         })?;
+        let mut temp_file = File::create(temp_file_path)
+            .map_err(|e| format!("Failed to write trust settings: {}", e))?;
+        to_writer_xml(&mut temp_file, &plist_root)
+            .map_err(|e| format!("Failed to serialize trust settings: {}", e))?;
 
-//         let status = Command::new("sudo")
-//             .args(&[
-//                 "security",
-//                 "trust-settings-export",
-//                 "-d",
-//                 temp_file.path().to_str().unwrap(),
-//             ])
-//             .status()
-//             .map_err(|err| {
-//                 TrustStoreError::CommandError(format!("Failed to export trust settings: {}", err))
-//             })?;
+        let output = Command::new("sudo")
+            .arg("security")
+            .arg("trust-settings-import")
+            .arg("-d")
+            .arg(temp_file_path)
+            .output()
+            .map_err(|e| format!("Failed to execute security trust-settings-import: {}", e))?;
 
-//         if !status.success() {
-//             return Err(TrustStoreError::CommandError(
-//                 "trust-settings-export command failed".to_string(),
-//             ));
-//         }
+        if !output.status.success() {
+            return Err(format!(
+                "Error executing security trust-settings-import: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ));
+        }
 
-//         let mut plist_data = Vec::new();
-//         temp_file.read_to_end(&mut plist_data).map_err(|err| {
-//             TrustStoreError::CommandError(format!("Failed to read trust settings: {}", err))
-//         })?;
+        fs::remove_file(temp_file_path).ok();
 
-//         let mut plist_root: Value = plist::from_bytes(&plist_data).map_err(|err| {
-//             TrustStoreError::CommandError(format!("Failed to parse plist: {}", err))
-//         })?;
+        Ok(true)
+    }
 
-//         self.modify_trust_settings(&mut plist_root, cert_path)?;
+    pub fn uninstall_platform(&self) -> Result<bool, String> {
+        let cert_path = format!("{}/{}", self.ca_root, "rootCA.pem");
+        let output = Command::new("sudo")
+            .arg("security")
+            .arg("remove-trusted-cert")
+            .arg("-d")
+            .arg(&cert_path)
+            .output()
+            .map_err(|e| format!("Failed to execute security remove-trusted-cert: {}", e))?;
 
-//         let updated_data = plist::to_bytes(&plist_root).map_err(|err| {
-//             TrustStoreError::CommandError(format!("Failed to serialize plist: {}", err))
-//         })?;
-//         temp_file.write_all(&updated_data).map_err(|err| {
-//             TrustStoreError::CommandError(format!(
-//                 "Failed to write updated trust settings: {}",
-//                 err
-//             ))
-//         })?;
+        if !output.status.success() {
+            return Err(format!(
+                "Error executing security remove-trusted-cert: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ));
+        }
 
-//         let status = Command::new("sudo")
-//             .args(&[
-//                 "security",
-//                 "trust-settings-import",
-//                 "-d",
-//                 temp_file.path().to_str().unwrap(),
-//             ])
-//             .status()
-//             .map_err(|err| {
-//                 TrustStoreError::CommandError(format!(
-//                     "Failed to import updated trust settings: {}",
-//                     err
-//                 ))
-//             })?;
-
-//         if !status.success() {
-//             return Err(TrustStoreError::CommandError(
-//                 "trust-settings-import command failed".to_string(),
-//             ));
-//         }
-
-//         Ok(())
-//     }
-
-//     fn modify_trust_settings(
-//         &self,
-//         plist_root: &mut Value,
-//         _cert_path: &Path,
-//     ) -> Result<(), TrustStoreError> {
-//         let root_subject_asn1 = self.certificate_subject_asn1()?;
-
-//         if let Some(plist_dict) = plist_root.as_dictionary_mut() {
-//             // Ensure trust settings version is 1, as in the Go code
-//             if let Some(Value::Integer(trust_version)) = plist_dict.get("trustVersion") {
-//                 if *trust_version != 1 {
-//                     return Err(TrustStoreError::CommandError(format!(
-//                         "Unsupported trust settings version: {}",
-//                         trust_version
-//                     )));
-//                 }
-//             }
-
-//             // Get the trustList entry and check for matching issuerName
-//             if let Some(Value::Dictionary(trust_list)) = plist_dict.get_mut("trustList") {
-//                 for (key, entry) in trust_list.iter_mut() {
-//                     if let Some(entry_dict) = entry.as_dictionary_mut() {
-//                         if let Some(Value::Data(issuer_name)) = entry_dict.get("issuerName") {
-//                             if issuer_name == &root_subject_asn1 {
-//                                 // Match found; modify the trust settings
-//                                 entry_dict.insert(
-//                                     "trustSettings".to_string(),
-//                                     Value::Array(vec![
-//                                         Value::Dictionary(HashMap::from([
-//                                             (
-//                                                 "kSecTrustSettingsPolicy".to_string(),
-//                                                 Value::Data(vec![
-//                                                     0x2a, 0x03, 0x28, 0x72, 0x36, 0x0e,
-//                                                 ]),
-//                                             ),
-//                                             (
-//                                                 "kSecTrustSettingsPolicyName".to_string(),
-//                                                 Value::String("sslServer".to_string()),
-//                                             ),
-//                                             (
-//                                                 "kSecTrustSettingsResult".to_string(),
-//                                                 Value::Integer(1),
-//                                             ),
-//                                         ])),
-//                                         Value::Dictionary(HashMap::from([
-//                                             (
-//                                                 "kSecTrustSettingsPolicy".to_string(),
-//                                                 Value::Data(vec![
-//                                                     0x2a, 0x03, 0x28, 0x72, 0x36, 0x0c,
-//                                                 ]),
-//                                             ),
-//                                             (
-//                                                 "kSecTrustSettingsPolicyName".to_string(),
-//                                                 Value::String("basicX509".to_string()),
-//                                             ),
-//                                             (
-//                                                 "kSecTrustSettingsResult".to_string(),
-//                                                 Value::Integer(1),
-//                                             ),
-//                                         ])),
-//                                     ]),
-//                                 );
-//                                 return Ok(()); // Trust settings modified successfully
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         Err(TrustStoreError::CommandError(
-//             "Failed to find matching trust settings entry".to_string(),
-//         ))
-//     }
-
-//     fn certificate_subject_asn1(&self) -> Result<Vec<u8>, TrustStoreError> {
-//         let subject_name: X509Name = self.certificate.subject_name().to_owned().unwrap();
-//         subject_name
-//             .to_der()
-//             .map_err(|err| TrustStoreError::PEMEncodingError(err.into()))
-//     }
-// }
+        Ok(true)
+    }
+}
